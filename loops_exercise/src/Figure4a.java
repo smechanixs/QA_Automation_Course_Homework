d5964a5c4edc8fb22e420b6bfc1dc5232cea4e95
Marin Ivanov <marin.solars@gmail.com>
2019-05-20 17:54:30 +0300

    .

---
+++ loops_exercise/src/Figure4a.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Figure4a {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a number for height and width: ");
+        int num = sc.nextInt();
+
+    }
+}
